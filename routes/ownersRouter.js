c1ce5d44efd685963f0f71f7efe90f4d76ad572d
aditya <adityapiyush71@gmail.com>
2025-08-23 19:22:33 +0530

    .

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -2,14 +2,27 @@ const express = require('express')
 const router = express.Router()
 const ownerModel = require('../models/owner-model')
 
-router.get("/", (req, res) => {
-})
 
 if(process.env.NODE_ENV === "development") {
-    router.post("/create", (req, res) => {
-        res.send("heyy")
+    router.post("/create", async (req, res) => {
+        let owners = await ownerModel.find()
+        if(owners.length > 0) return res.status(500).send("You don't have the permission to create an owner.")
+
+        let { fullname, email, password } = req.body
+        
+        let createdOwner = await ownerModel.create({
+            fullname,
+            email,
+            password
+        })
+        res.status(201).send(createdOwner)
     })
 }
+
+router.get("/", (req, res) => {
+
+})
+
 
 
 module.exports = router
