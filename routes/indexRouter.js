ad9e405781f06771420c6f507385729ad8c28370
aditya <adityapiyush71@gmail.com>
2025-08-24 21:16:38 +0530

    update

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,15 +2,20 @@ const express = require('express')
 const router =  express.Router()
 const isLoggedIn = require('../middlewares/isLoggedIn')
 const productModel = require('../models/product-model')
+const userModel = require('../models/user-model')
 
 router.get("/", function(req,res) {
     let error = req.flash("error")
-    res.render("index", { error })
+    res.render("index", { error, loggedin: false })
 })
 
 router.get("/shop", isLoggedIn, async function(req, res) {
     let products = await productModel.find()
     res.render("shop", { products })
+})
+
+router.get("/addtocart/:id", isLoggedIn, async function(req, res) {
+    userModel.findOneAndUpdate({ email: req.user.email }, { $push: { cart: req.params.id } })
 })
 
 router.get("/logout", isLoggedIn, function(req, res) {
